eaa6859fb73d0cbb02d5a86ca08c981cb9e29275
Adrian <48166141+0110lekniw@users.noreply.github.com>
2020-07-23 13:41:42 +0100

    Finished Section 3

---
+++ 2_Primitives/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primitives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Primitive Types
+            byte b = 2;
+            int i = 10;
+            float f = 2.85f;
+            char character = 'A';
+            string str = "Adrian";
+            bool boolean = true;
+            const float Pi = 3.14f;
+
+            //max min value
+            Console.WriteLine("{0}{1}", float.MinValue, float.MaxValue);
+
+            //Conversion
+
+            //Implicit
+            byte number1 = 1;
+            int number2 = number1;
+            //Explicit
+            int number3 = 123;
+            byte number4 = (byte)number3;
+            //Conversion
+            string number = "1234";
+            int number5 = Convert.ToInt32(number);
+
+            //Exception Error 
+            try
+            {
+                byte smallNumber = Convert.ToByte(number);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Cannot Convert to Byte");
+            }
+
+        }
+    }
+}
---
+++ 3_operators/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[])
+        {
+            var a = 10;
+            var b = 3;
+            var c = 2;
+
+            //Mathematical Operators
+
+            //Adding
+            Console.WriteLine(a + b);
+            //Dividing - integer result
+            Console.WriteLine(a / b);
+            //Dividing - float result
+            Console.WriteLine(a / (float)b);
+            // Operators Precedence
+            Console.WriteLine(a + b * c);
+
+            //Logical Operators
+
+            //Compairment
+            Console.WriteLine(a > b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            //NOT
+            Console.WriteLine(!(a != b));
+            //AND
+            Console.WriteLine(c > b && c < a);
+            //OR
+            Console.WriteLine(c > b || c < a);
+
+        }
+    }
+}
