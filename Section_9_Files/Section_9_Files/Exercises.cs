d04a3e12723623279893b5c27f083cc76af247b7
Adrian <chojnacki.adrian@yahoo.com>
2020-07-29 18:58:48 +0200

    Finished Section 9

---
+++ Section_9_Files/Section_9_Files/Exercises.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_9_Files
+{
+    class Exercises
+    {
+        public static void ExerciseOne()
+        {
+            var path = @"c:\GitHub\Csharp_Basics\Section_9_Files";
+            var directories = Directory.GetFiles(path, "*.txt", SearchOption.AllDirectories);
+
+            string filePath = path;
+            string textFile = path;
+
+            for(var i = 0; i < directories.Length; i++)
+                if (directories[i].Contains("Text"))
+                    filePath = directories[i];
+
+            if (File.Exists(filePath))
+                textFile = File.ReadAllText(filePath);
+
+            var words = textFile.Split(' ');
+            Console.WriteLine("Number of Words is " + words.Count());
+
+            var longestWord = words[0];
+            foreach (var word in words)
+                if (longestWord.Length < word.Length)
+                    longestWord = word;
+            Console.WriteLine("The longest word is: " + longestWord);
+
+        }
+    }
+}
---
+++ Section_9_Files/Section_9_Files/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_9_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercises.ExerciseOne();
+        }
+
+        static void DemoFiles()
+        {
+            var path = @"c:\somefile.jpg";
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
+        }
+
+        static void DemoDirectory()
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+            var files = Directory.GetFiles(@"C:\Users\chojn", "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
+                Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\Users\chojn", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+
+        static void DemoPath()
+        {
+            var path = @"c:\somefile.jpg";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            Console.WriteLine("File Name without Extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+
+
+        }
+    }
+}
